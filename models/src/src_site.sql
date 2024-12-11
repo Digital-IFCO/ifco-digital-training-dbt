946abb722d66ba25f95940d4a03f3c333ffe3f67
danisoronellas <daniel.soronellas@gmail.com>
2024-12-11 19:30:17 +0100

    completed step 4 practice

---
+++ models/src/src_site.sql
@@ -0,0 +1,13 @@
+with raw_site as (
+    select *
+    from training_dbt.raw.raw_site
+)
+
+select
+    Id as site_id,
+    SiteCode as site_code,
+    Name as site_name
+from raw_site
+where
+    historyStatus = 'A'
+    and validTo_DLS = '9999-12-31T23:59:59.999+00:00'
