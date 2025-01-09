95718fdbbd59e0b896d288d737d19cf61ef85c56
Rodrigo Martín Soto <25295816+gotinso@users.noreply.github.com>
2025-01-09 10:40:45 +0100

    added two initial notebooks for the demo

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Starting point
+
+## Demo - Creating a Bundle from Scratch
+This demo will closely follow
+https://learn.microsoft.com/en-us/azure/databricks/dev-tools/bundles/manual-bundle
+
+### Steps
+- Make sure we are starting from a branch without unrelated files.
+- Review the two given notebooks.
+- 
+  
---
+++ transform_crate_data.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+
+# Import necessary libraries
+from pyspark.sql.functions import col, sum
+
+# Read the crate data table
+df = spark.table("training_dbt.s3_dab_schema.crate_data")
+
+# Perform analysis: Calculate total quantity per crate_id
+agg_df = df.groupBy("crate_id").agg(sum("quantity").alias("total_quantity"))
+
+# Write the aggregated DataFrame to a new table in Unity Catalog
+agg_df.write.mode("overwrite").saveAsTable("training_dbt.s3_dab_schema.crate_data_summary")
