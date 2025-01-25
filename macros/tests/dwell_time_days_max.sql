0ff5fc031f04a1afad5a3cec548f7db0d3921956
Pablo <pablo.beamud@ifco.com>
2025-01-25 15:10:21 +0100

    s5-exercise: add a generic data test

---
+++ macros/tests/dwell_time_days_max.sql
@@ -0,0 +1,17 @@
+{% test dwell_time_days_max(model, max_value) %}
+
+/*
+  This test checks that no records in the specified model have a `dwellTimeDays` value exceeding `max_value`.
+  If any such records exist, the test will fail and return the `locationId` and `visitId` of those records.
+*/
+
+with violations as (select locationId,
+                           visitId,
+                           dwellTimeDays
+                    from {{ model }}
+                    where dwellTimeDays > {{ max_value }})
+
+select *
+from violations
+
+{% endtest %}
